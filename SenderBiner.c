f9712d26ac01cf1939a8d154b3e6c195f14fe33e
Pahala Putra Tambunan <151481125+spearofbonedragonz@users.noreply.github.com>
2023-11-22 21:13:49 +0700

    SenderBiner.c

---
+++ SenderBiner.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void decToBinary(int n){
+  int binaryNum[32]; //ukuran array yang cukup besar untuk menyimpan representasi biner
+
+  int i = 0; //Indeks yang menunjukkan posisi saat ini dalam array binerNum
+
+  while(n>0){
+  binaryNum[i] = n % 2;
+  n = n / 2;
+  i++;
+  }
+
+  //menampilkan representasi biner dari bilangan desimal
+  printf("Representasi biner : ");
+  for(int j = i - 1; j >= 0; j--){
+      printf("%d", binaryNum[j]);
+  }
+  printf("\n");
+
+}
+int main(){
+   int decimalNumber;
+  
+  printf("Masukkan bilangan desimal : ");
+  scanf("%d", &decimalNumber);
+
+  decToBinary(decimalNumber);
+
+return 0;
+}
+  
